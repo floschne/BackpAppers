60b10556c673a52421725553b2cccbede7a65564
Flo <p0w3r@v22017044496947516.goodsrv.de>
2017-09-28 00:36:39 +0200

    Initial Maven SpringBoot project setup

---
+++ src/main/java/de/p0w3rhub/backpAppers/BackpAppersApplication.java
@@ -0,0 +1,12 @@
+package de.p0w3rhub.backpAppers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpAppersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpAppersApplication.class, args);
+	}
+}
---
+++ src/test/java/de/p0w3rhub/backpAppers/BackpAppersApplicationTests.java
@@ -0,0 +1,16 @@
+package de.p0w3rhub.backpAppers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackpAppersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
